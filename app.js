dab3e0f8234c56afcd40364db40dbd6a3abc5cc5
RJRuhan <rizvanjawad@gmail.com>
2023-12-11 00:42:19 +0600

    **

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const upload = multer({ dest: 'dataset/' })
 const fs = require('node:fs');
 
-const {pgp,db} = require("./db");
+const { pgp, db } = require("./db");
 
 const dotenv = require('dotenv');
 const authenticateKey = require("./apiAuth");
@@ -18,25 +18,33 @@ app.use(express.json());
 
 
 
-app.post('/addNewData',authenticateKey,upload.single('avatar'), async (req, res) => {
+app.post('/addNewData', authenticateKey, upload.single('avatar'), async (req, res) => {
 
     let data = ''
     try {
-    data = fs.readFileSync(req.file.path, 'utf8');
+        data = fs.readFileSync(req.file.path, 'utf8');
     } catch (err) {
-    console.error(err);
+        console.error(err);
+        res.status(400).json({ message: 'Internal Server Error' });
+        return
     }
 
     // delete a file asynchronously
     fs.unlink(req.file.path, (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('File is deleted.');
-    }
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('File is deleted.');
+        }
     });
-    
-    data = JSON.parse(data)
+
+    try {
+        data = JSON.parse(data)
+    } catch (error) {
+        console.error('Json parse Error: ', error);
+        res.status(400).json({ message: 'Bad Data' });
+        return
+    }
 
     try {
 
@@ -49,7 +57,7 @@ app.post('/addNewData',authenticateKey,upload.single('avatar'), async (req, res)
 
         const walks = data.walks;
         // walks = walks.map(obj => ({ ...obj, subject_id : subject_id, phone_model : "dummy", walk_type : 0 }));
-        for (let i = 0;i < walks.length;i++ ) {
+        for (let i = 0; i < walks.length; i++) {
             walks[i].subject_id = subject_id;
         }
 
@@ -58,23 +66,23 @@ app.post('/addNewData',authenticateKey,upload.single('avatar'), async (req, res)
         const walk_id = result = await db.many(query);
         console.log('Inserted row', walk_id);
 
-        for(let i = 0;i < walks.length;i++){
+        for (let i = 0; i < walks.length; i++) {
             const mAccl = walks[i].mAccl;
             mAccl.walk_id = walk_id[i].id;
 
-            cs = new pgp.helpers.ColumnSet(['avg_sample_rate','avg_delay','sensor_model','accl_net_avg','accl_net_dev',
-            'accl_x_avg','accl_y_avg','accl_z_avg','accl_x_dev','accl_y_dev','accl_z_dev','walk_id'], { table: 'accelerometer' });
+            cs = new pgp.helpers.ColumnSet(['avg_sample_rate', 'avg_delay', 'sensor_model', 'accl_net_avg', 'accl_net_dev',
+                'accl_x_avg', 'accl_y_avg', 'accl_z_avg', 'accl_x_dev', 'accl_y_dev', 'accl_z_dev', 'walk_id'], { table: 'accelerometer' });
             query = pgp.helpers.insert(mAccl, cs) + ' RETURNING id';
             result = await db.one(query);
             const mAccl_id = result.id;
             console.log('Inserted row', mAccl_id);
 
             mAccl_data = mAccl.data
-            for(let i = 0;i < mAccl_data.length;i++){
+            for (let i = 0; i < mAccl_data.length; i++) {
                 mAccl_data[i].accelerometer_id = mAccl_id;
             }
-            
-            cs = new pgp.helpers.ColumnSet(['timestamp','accl_x','accl_y','accl_z','accelerometer_id'], { table: 'accelerometer_data' });
+
+            cs = new pgp.helpers.ColumnSet(['timestamp', 'accl_x', 'accl_y', 'accl_z', 'accelerometer_id'], { table: 'accelerometer_data' });
             query = pgp.helpers.insert(mAccl_data, cs) + ' RETURNING id';
             result = await db.many(query);
             console.log('Inserted row', result);
@@ -82,26 +90,26 @@ app.post('/addNewData',authenticateKey,upload.single('avatar'), async (req, res)
             const mGyro = walks[i].mGyro;
             mGyro.walk_id = walk_id[i].id;
 
-            cs = new pgp.helpers.ColumnSet(['avg_sample_rate','avg_delay','sensor_model','gyro_net_avg','gyro_net_dev',
-            'gyro_x_avg','gyro_y_avg','gyro_z_avg','gyro_x_dev','gyro_y_dev','gyro_z_dev','walk_id'], { table: 'gyroscope' });
+            cs = new pgp.helpers.ColumnSet(['avg_sample_rate', 'avg_delay', 'sensor_model', 'gyro_net_avg', 'gyro_net_dev',
+                'gyro_x_avg', 'gyro_y_avg', 'gyro_z_avg', 'gyro_x_dev', 'gyro_y_dev', 'gyro_z_dev', 'walk_id'], { table: 'gyroscope' });
             query = pgp.helpers.insert(mGyro, cs) + ' RETURNING id';
             result = await db.one(query);
             const mGyro_id = result.id;
             console.log('Inserted row', mGyro_id);
 
             mGyro_data = mGyro.data
-            for(let i = 0;i < mGyro_data.length;i++){
+            for (let i = 0; i < mGyro_data.length; i++) {
                 mGyro_data[i].gyroscope_id = mGyro_id;
             }
-            
-            cs = new pgp.helpers.ColumnSet(['timestamp','gyro_x','gyro_y','gyro_z','gyroscope_id'], { table: 'gyroscope_data' });
+
+            cs = new pgp.helpers.ColumnSet(['timestamp', 'gyro_x', 'gyro_y', 'gyro_z', 'gyroscope_id'], { table: 'gyroscope_data' });
             query = pgp.helpers.insert(mGyro_data, cs) + ' RETURNING id';
             result = await db.many(query);
             console.log('Inserted row', result);
         }
 
         res.status(201).json({ message: 'Data inserted successfully' });
-        
+
     } catch (error) {
         console.error('Error inserting data:', error);
         res.status(500).json({ message: 'Internal Server Error' });
